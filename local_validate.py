21da320b90fa96d494cf0bc682c8e55c1bf61a0f
J. Seth Strattan <jseth@stanford.edu>
2013-10-30 18:13:34 -0700

    Script to validate a JSON object against its schema.

---
+++ local_validate.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+'''Validate an ENCODE JSON object'''
+
+'''use requests to handle the HTTP connection'''
+import requests
+'''use json to convert between Python dictionaries and JSON objects'''
+import json
+'''use jsonschema to validate objects against the JSON schemas'''
+import jsonschema
+import sys, os.path
+
+EPILOG = __doc__
+
+'''store the ENCODE server address and an authorization keypair'''
+'''create the keypair from persona or get one from your wrangler'''
+SERVER = 'http://test.encodedcc.org'
+AUTHID = 'access_key_id'
+AUTHPW = 'secret_access_key'
+'''force return from the server in JSON format'''
+HEADERS = {'content-type': 'application/json'}
+
+def get_ENCODE(obj_id):
+    '''GET an ENCODE object as JSON and return as dict'''
+    url = SERVER+obj_id+'?limit=all'
+    response = requests.get(url, auth=(AUTHID, AUTHPW), headers=HEADERS)
+    if not response.status_code == 200:
+        print >> sys.stderr, response.text
+    return response.json()
+
+def patch_ENCODE(obj_id, patch_json):
+    '''PATCH an existing ENCODE object and return the response JSON'''
+    url = SERVER+obj_id
+    json_payload = json.dumps(patch_json)
+    response = requests.patch(url, auth=(AUTHID, AUTHPW), data=json_payload)
+    print "Patch:"
+    print response.status_code
+    if not response.status_code == 200:
+        print >> sys.stderr, response.text
+    return response.json()
+
+def new_ENCODE(collection_id, object_json):
+    '''POST an ENCODE object as JSON and return the resppnse JSON'''
+    url = SERVER+collection_id
+    json_payload = json.dumps(object_json)
+    response = requests.post(url, auth=(AUTHID, AUTHPW), headers=HEADERS, data=json_payload)
+    if not response.status_code == 201:
+        print >> sys.stderr, response.text
+    return response.json()
+
+def processkeys(args):
+
+	keysf = open(args.keyfile,'r')
+	keys_json_string = keysf.read()
+	keysf.close()
+	keys = json.loads(keys_json_string)
+	key_dict = keys[args.key]
+	global AUTHID
+	global AUTHPW
+	global SERVER
+	if not args.authid:
+		AUTHID = key_dict['key']
+	else:
+		AUTHID = args.authid
+	if not args.authpw:
+		AUTHPW = key_dict['secret']
+	else:
+		AUTHPW = args.authpw
+	if not args.server:
+		SERVER = key_dict['server']
+	else:
+		SERVER = args.server
+	if not SERVER.endswith("/"):
+		SERVER += "/"
+
+def main():
+
+	import argparse
+	parser = argparse.ArgumentParser(
+	    description="Validate ENCODE JSON object against the appropriate ENCODE schema", epilog=EPILOG,
+	    formatter_class=argparse.RawDescriptionHelpFormatter,
+	)
+
+	parser.add_argument('--infile', '-i',
+		help="File containing the JSON object as a JSON string.")
+	parser.add_argument('--server',
+		help="Full URL of the server.")
+	parser.add_argument('--key',
+		default='default',
+		help="The keypair identifier from the keyfile.  Default is --key=default")
+	parser.add_argument('--keyfile',
+		default=os.path.expanduser("~/keypairs.json"),
+		help="The keypair file.  Default is --keyfile=%s" %(os.path.expanduser("~/keypairs.json")))
+	parser.add_argument('--authid',
+		help="The HTTP auth ID.")
+	parser.add_argument('--authpw',
+		help="The HTTP auth PW.")
+	parser.add_argument('--debug',
+		default=False,
+		action='store_true',
+		help="Print debug messages.  Default is False.")
+	args = parser.parse_args()
+
+	processkeys(args)
+
+	if args.infile:
+		infile = open(args.infile,'r')
+	else:
+		infile = sys.stdin
+
+	json_string = infile.read()
+	json_object = json.loads(json_string)
+
+	try:
+		datatype = json_object.pop('@type')[0]
+	except:
+		print "No datatype in @type property"
+		raise
+
+	schema_uri = '/profiles/' + datatype + '.json'
+	object_schema = get_ENCODE(schema_uri)
+	schema_fields = dict.fromkeys(object_schema['properties'].keys())
+
+	jsonschema.validate(json_object, object_schema)
+	print "Validation passed"
+
+if __name__ == '__main__':
+	main()
