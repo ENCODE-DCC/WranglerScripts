c208f921e2305a79f2022dc4e20623059378b219
Drew Erickson <drew.t.erickson@stanford.edu>
2013-11-19 14:28:49 -0800

    Update cells
    added basic process for updating a cell or list of cells.

--- GDataRW.py
+++ GDataRW.py
@@ -7,6 +7,8 @@ password = "abcxyz"
 spreadname = 'Import Test Sheet'
 workname = 'antitestset'
 
+updates = {'B4':'21','B6':'69'}
+
 
 # start a connection
 sheetclient = gdata.spreadsheet.service.SpreadsheetsService()
@@ -36,17 +38,33 @@ for row in rows:
         print " %s: %s" % (key, row.custom[key].text)
     print
 
-# retrieve data as cells
-query = gdata.spreadsheet.service.CellQuery()
-query.return_empty = "true" 
-cells = sheetclient.GetCellsFeed(spreadid,workid,query=query)
-
-
 # insert a new row (does not check for duplicate)
+# (column names must be lowercase, spaces removed)
+# if column doesn't match existing column, no patch is done for that value
 dicti = {}
 dicti['bizarroname'] = 'AAA'
 dicti['bizzarodepleted'] = '42' 
 dicti['broken'] = 'no'
 entry = sheetclient.InsertRow(dicti, spreadid, workid)
 
+# retrieve data as cells
+query = gdata.spreadsheet.service.CellQuery()
+query.return_empty = "true" 
+cells = sheetclient.GetCellsFeed(spreadid,workid,query=query)
 
+# update cell.  based on coordinate system on spreadsheet (column letter, row number)
+# could also use col/row numbers by looking in entry.cell.row and entry.cell.col
+# normal query doesn't work for GetCellsFeed (even though similar class structure)
+#query = gdata.spreadsheet.service.DocumentQuery()
+#query.title = cellname
+#query.title_exact = "true"
+query = gdata.spreadsheet.service.CellQuery()
+query.return_empty = "true" 
+cells = sheetclient.GetCellsFeed(spreadid,workid,query=query)
+action = gdata.spreadsheet.SpreadsheetsCellsFeed()
+for entry in cells.entry:
+    for cellname in updates.keys():
+        if entry.title.text == cellname:
+            entry.cell.inputValue = updates[cellname]
+            action.AddUpdate(entry)
+updated = sheetclient.ExecuteBatch(action, cells.GetBatchLink().href)
