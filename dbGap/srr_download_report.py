c39f0a5d2e349a1427bb178e6a85cb2964333edc
J. Seth Strattan <jseth@stanford.edu>
2017-10-27 16:45:35 -0700

    Report usefully on failed downloads

--- dbGap/srr_download_report.py
+++ dbGap/srr_download_report.py
@@ -41,39 +41,45 @@ def get_args():
 
 def main():
     args = get_args()
-    applets = list(dxpy.find_data_objects(classname='applet', name='dbgap_sra_to_fastq*', name_mode='glob', project=args.project, return_handler=True))
-    assert applets
+    # applets = list(dxpy.find_data_objects(classname='applet', name='dbgap_sra_to_fastq*', name_mode='glob', project=args.project, return_handler=True))
+    # assert applets
     srrs = args.SRRs or args.infile
     fieldnames = ['SRR', 'sra_size', 'sra_md5', 'fastq_id', 'fastq_alias', 'fastq_size', 'fastq_name', 'fastq_md5']
     writer = csv.DictWriter(args.outfile, fieldnames, delimiter='\t')
     writer.writeheader()
-    jobs = []
-    for applet in applets:
-        jobs.extend(list(dxpy.find_executions(executable=applet, describe=True)))
+    # jobs = []
+    # for applet in applets:
+    #     jobs.extend(list(dxpy.find_executions(executable=applet, describe=True, first_page_size=1000)))
     for row in srrs:
         if row.startswith('#'):
             continue
         srr = row.strip()
-        srr_jobs = [j['describe'] for j in jobs if j['describe'].get('state') == 'done' and srr in j['describe']['input']['SRR']]
+        srr_jobs = [j.get('describe') for j in dxpy.find_executions(describe=True, name="%s_sra_to_fastq" % (srr))]
+        # srr_jobs = [j['describe'] for j in jobs if j['describe'].get('state') == 'done' and srr in j['describe']['input']['SRR']]
         if not srr_jobs:
-            writer.writerow({'SRR': srr})
+            writer.writerow({'SRR': "%s: not downloaded" % (srr)})
         else:
-            for job in srr_jobs:
-                outrow = {
-                    'SRR': job['input'].get('SRR'),
-                    'sra_size': job['output'].get('sra_size'),
-                    'sra_md5': job['output'].get('sra_md5')
-                }
-                for i, fastq in enumerate(job['output'].get('fastq')):
-                    fh = dxpy.DXFile(job['output'].get('fastq')[i])
-                    outrow.update({
-                        'fastq_id': fh.get_id(),
-                        'fastq_alias': ":".join(["dnanexus", fh.get_id()]),
-                        'fastq_size': fh.describe().get('size'),
-                        'fastq_name': job['output'].get('fastq_filenames')[i],
-                        'fastq_md5': job['output'].get('fastq_md5s')[i]
-                    })
-                    writer.writerow(outrow)
+            if not any([job.get('state') == 'done' for job in srr_jobs]):
+                for job in srr_jobs:
+                    if job.get('state') != 'done':
+                        writer.writerow({'SRR': "%s: job %s %s" %(srr, job.get('id'), job.get('state'))})
+            else:
+                for job in [j for j in srr_jobs if j.get('state') == 'done']:
+                    outrow = {
+                        'SRR': job['input'].get('SRR'),
+                        'sra_size': job['output'].get('sra_size'),
+                        'sra_md5': job['output'].get('sra_md5')
+                    }
+                    for i, fastq in enumerate(job['output'].get('fastq')):
+                        fh = dxpy.DXFile(job['output'].get('fastq')[i])
+                        outrow.update({
+                            'fastq_id': fh.get_id(),
+                            'fastq_alias': ":".join(["dnanexus", fh.get_id()]),
+                            'fastq_size': fh.describe().get('size'),
+                            'fastq_name': job['output'].get('fastq_filenames')[i],
+                            'fastq_md5': job['output'].get('fastq_md5s')[i]
+                        })
+                        writer.writerow(outrow)
 
 
 if __name__ == '__main__':
